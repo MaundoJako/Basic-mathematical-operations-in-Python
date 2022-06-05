77ae98b2284165b9cc534d2ce64acd19d3f37717
Jake <91381193+MaundoJako@users.noreply.github.com>
2022-06-05 15:14:44 +0000

    added teahcers bit

--- main.py
+++ main.py
@@ -50,3 +50,12 @@ months_had = (new_age * 12)
 months_left = (months_90 - months_had)
 #print(months_left)
 print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left")
+
+#Notes from teach
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+message = f"You have {days_remaining} days, {weeks_remaining} weeks and {months_remaining} months left"
+print(message) 
