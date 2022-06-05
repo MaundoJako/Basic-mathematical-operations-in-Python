08448c7cd0072aa96ae9941ade66e985336f9c26
Jake <91381193+MaundoJako@users.noreply.github.com>
2022-06-05 15:05:36 +0000

    Added task and f strings

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
-# D2/L22 -  Mathematical Operations in Python
-print(type(3*6))
-#Gives u the type
+#### Number Manipulation and F Strings in Python
+print(round(8/3, 2))
+#### comma is the decimel points
+print(8 // 3)
+#### // flaw division - calculation will always be converted into a float. But if u use // then it gets put into an int.
 
-print (2**2)
-#Gives the power to ---- 2 * 2 * 2
+result = 4 / 2
+result /= 2
+print(result)
+#### this divides by 2, then divides by 2 again
 
-#TASK - Calculating BMI
+#### example
+score = 0
+#### user scores a point
+score += 1
+print(score)
+#### relly useful for manipulating a value based jupon its previous value
+
+#### F Strings
+score = 0
+height = 1.8
+is_winning = True
+print(f"your score is {score}, your height is {height}, you are winning is {is_winning}")
+
+#### add f infront of "" to make it into an f string
+#### Does all the converting for you
+
+
+#### TASK - calculating how many days, weeks and months left till you turn 90
 
 # 🚨 Don't change the code below 👇
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
 
-height1 = float(height)
-weight1 = int(weight)
+new_age = int(age)
 
-result = int(weight1 / (height1 * height1))
+days_90 = (365 * 90)
+days_had = (new_age * 365)
+days_left = (days_90 - days_had)
+#print(days_left)
 
-print(result)
-
-# OR
-
-#bmi = int(weight) / float(height) ** 2 
-#bmi_int = int(bmi)
-#print(bmi_int)
+weeks_90 = (52 * 90)
+weeks_had = (new_age * 52)
+weeks_left = (weeks_90 - weeks_had)
+#print(weeks_left)
+months_90 = (12 * 90)
+months_had = (new_age * 12)
+months_left = (months_90 - months_had)
+#print(months_left)
+print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left")
